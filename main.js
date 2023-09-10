2e8c0ae10850f8ed91e17910af38c17d15c75866
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-09-10 21:14:47 +0800

    feat: Bypass Text Restriction

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 const canvas = document.body.appendChild(document.createElement("canvas"));
 const debugCanvas = false; //will show the canvas on the page
 
-if(debugCanvas){
-    canvas.style.position = "absolute";
-    // center
-    canvas.style.left = "50%";
-    canvas.style.top = "50%";
-    canvas.style.transform = "translate(-50%, -50%)";
-    // border
-    canvas.style.border = "1px solid red";
+if (debugCanvas) {
+  canvas.style.position = "absolute";
+  // center
+  canvas.style.left = "50%";
+  canvas.style.top = "50%";
+  canvas.style.transform = "translate(-50%, -50%)";
+  // border
+  canvas.style.border = "1px solid red";
 }
 
 // Convert react input into html input tag
@@ -24,7 +24,7 @@ textInput.parentNode.replaceChild(newTextInput, textInput);
 // Change the canvas size here
 let resFactor = 30;
 canvas.width = 16 * resFactor;
-canvas.height = 9 * resFactor; 
+canvas.height = 9 * resFactor;
 
 // Get the 2D rendering context
 const ctx = canvas.getContext("2d");
@@ -68,26 +68,26 @@ const inputElement = document.querySelector(
 
 function sendTextToImage(text) {
   const createdFile = textToFile(text);
-  const dataTransfer = new DataTransfer();
-  dataTransfer.items.add(createdFile);
-  const event = new Event("change", {
-    bubbles: true,
-    cancelable: true,
+  const pasteEvent = new ClipboardEvent('paste', {
+    clipboardData: new DataTransfer(),
+    target: inputElement
   });
-  inputElement.files = dataTransfer.files;
-  inputElement.dispatchEvent(event);
+  pasteEvent.clipboardData.items.add(createdFile);
+  document.dispatchEvent(pasteEvent);
+
+
   setTimeout(() => {
     const sendButton = document.querySelector("body > div.popup.popup-send-photo.popup-new-media.active > div > div.popup-input-container > button")
     sendButton.click();
-    }, 30);
+  }, 30);
 }
 
 
 newTextInput.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-        sendTextToImage(newTextInput.value);
-        newTextInput.value = "";
-    }
+  if (e.key === "Enter") {
+    sendTextToImage(newTextInput.value);
+    newTextInput.value = "";
+  }
 });
 
 // sendTextToImage("Hello World");
